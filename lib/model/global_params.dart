4266a53c716daa6fac65a91c14810307f31995aa
Nathan Kamokoue <sikatinathan@gmail.com>
2024-02-19 20:31:21 +0100

    changed baseUrl to remote url

--- lib/model/global_params.dart
+++ lib/model/global_params.dart
@@ -1,4 +1,4 @@
-String baseUrl = 'http://localhost:8000';
-// String baseUrl = "https://helpgenius-api-iai7unx7la-uc.a.run.app";
+// String baseUrl = 'http://localhost:8000';
+String baseUrl = "https://helpgenius-api-iai7unx7la-uc.a.run.app";
 bool authenticated = false;
 String email = '';
